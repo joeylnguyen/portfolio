488153f3fb6f2c1134dcc8db8a4b762f2decfa67
Joey Nguyen <joeylnguyen95@gmail.com>
2020-09-10 17:51:07 -0700

    Image paths...again

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -5,10 +5,10 @@ const projectData = [
     "githubLink": "https://github.com/joeylnguyen/playtime",
     "projectLink": "https://spotify-playtime.herokuapp.com",
     "technologies": ["React", "Styled Components", "Node.js/Express", "Spotify Web API"],
-    "heroImageLarge": '/joeylnguyen/portfolio/tree/gh-pages/images/playtimeImageLarge.PNG',
-    "heroImageSmall": '/joeylnguyen/portfolio/tree/gh-pages/images/playtimeImageSmall.PNG',
-    "heroImageLargeWebp": '/joeylnguyen/portfolio/tree/gh-pages/images/playtimeImageLarge.WEBP',
-    "heroImageSmallWebp": '/joeylnguyen/portfolio/tree/gh-pages/images/playtimeImageSmall.WEBP'
+    "heroImageLarge": '/images/playtimeImageLarge.PNG',
+    "heroImageSmall": '/images/playtimeImageSmall.PNG',
+    "heroImageLargeWebp": '/images/playtimeImageLarge.WEBP',
+    "heroImageSmallWebp": '/images/playtimeImageSmall.WEBP'
 
   },
   {
@@ -17,10 +17,10 @@ const projectData = [
     "githubLink": "https://github.com/LeggoMyLego/ProductComponent",
     "projectLink": "https://github.com/LeggoMyLego/ProductComponent",
     "technologies": ["React", "Styled Components", "Node.js/Express", "AWS EC2", "MySQL", "Jest", "Enzyme", "React Testing Library",],
-    "heroImageLarge": '/joeylnguyen/portfolio/tree/gh-pages/images/leggoImageLarge.PNG',
-    "heroImageSmall": '/joeylnguyen/portfolio/tree/gh-pages/images/leggoImageSmall.PNG',
-    "heroImageLargeWebp": '/joeylnguyen/portfolio/tree/gh-pages/images/leggoImageLarge.WEBP',
-    "heroImageSmallWebp": '/joeylnguyen/portfolio/tree/gh-pages/images/leggoImageSmall.WEBP'
+    "heroImageLarge": '/images/leggoImageLarge.PNG',
+    "heroImageSmall": '/images/leggoImageSmall.PNG',
+    "heroImageLargeWebp": '/images/leggoImageLarge.WEBP',
+    "heroImageSmallWebp": '/images/leggoImageSmall.WEBP'
   },
   {
     "name": "SnakeTours",
@@ -28,10 +28,10 @@ const projectData = [
     "githubLink": "https://github.com/MomoDB/snaketours-server",
     "projectLink": "https://github.com/MomoDB/snaketours-server",
     "technologies": ["Nginx", "Node.js/Express", "New Relic", "Loader.io", "Redis", "AWS Cloudwatch", "MongoDB"],
-    "heroImageLarge": '/joeylnguyen/portfolio/tree/gh-pages/images/snaketours.JPG',
-    "heroImageSmall": '/joeylnguyen/portfolio/tree/gh-pages/images/snaketours.JPG',
-    "heroImageLargeWebp": '/joeylnguyen/portfolio/tree/gh-pages/images/snaketours.WEBP',
-    "heroImageSmallWebp": '/joeylnguyen/portfolio/tree/gh-pages/images/snaketours.WEBP'
+    "heroImageLarge": '/images/snaketours.JPG',
+    "heroImageSmall": '/images/snaketours.JPG',
+    "heroImageLargeWebp": '/images/snaketours.WEBP',
+    "heroImageSmallWebp": '/images/snaketours.WEBP'
   }
 ];
 
