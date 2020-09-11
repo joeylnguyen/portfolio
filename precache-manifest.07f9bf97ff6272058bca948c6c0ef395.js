e54393834542a269bcbc31fc6646dffeb30d4c50
Joey Nguyen <joeylnguyen95@gmail.com>
2020-09-10 22:53:01 -0700

    Updates

---
+++ precache-manifest.07f9bf97ff6272058bca948c6c0ef395.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d414a15c2569053a0d657d4e6ae59d1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30ed37f67109ca1ed211",
+    "url": "/static/css/main.004bd6f5.chunk.css"
+  },
+  {
+    "revision": "5b1f62a531dc47a8ae1f",
+    "url": "/static/js/2.1c017ac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c017ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ed37f67109ca1ed211",
+    "url": "/static/js/main.4230e11b.chunk.js"
+  },
+  {
+    "revision": "0cf5b4e5129282245695",
+    "url": "/static/js/runtime-main.0a0ddeb2.js"
+  },
+  {
+    "revision": "473c56f3bb8722199842424c1300e32f",
+    "url": "/static/media/externalLink.473c56f3.svg"
+  },
+  {
+    "revision": "856181f3812b33d5cd24ee6cdc5808fa",
+    "url": "/static/media/githubLogo.856181f3.svg"
+  },
+  {
+    "revision": "3ce7327a3eb6dc93fcd9a0d976ed7983",
+    "url": "/static/media/linkedInLogo.3ce7327a.svg"
+  },
+  {
+    "revision": "1de522fa3eb6805a66eddd83575e3d98",
+    "url": "/static/media/portrait.1de522fa.JPG"
+  },
+  {
+    "revision": "d55d5e97225e477cb09e57357f2ba0b6",
+    "url": "/static/media/portrait.d55d5e97.WEBP"
+  },
+  {
+    "revision": "0bdef6b3cdc37e96a2e0937b850c8b6b",
+    "url": "/static/media/resume.0bdef6b3.pdf"
+  }
+]);
